6855ba86bfb3aaf71c8565d93fa03939601ee61c
NoorAlamSabbagh <alamrangrej787@gmail.com>
2026-03-27 18:31:28 +0530

    Next Basic

--- NextJs_By CoderGyan/my-next-app/app/page.tsx
+++ NextJs_By CoderGyan/my-next-app/app/page.tsx
@@ -1,13 +1,6 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div>
-      <nav className="flex gap-4">
-        <Link href="/">Logo</Link>
-        <Link href="/">Home</Link>
-        <Link href="/blog">Blog</Link>
-      </nav>
       <h1>Welcome to Next JS Blog App</h1>
     </div>
   );
